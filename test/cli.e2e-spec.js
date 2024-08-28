cccfed09fa049f8edc832390f6260438761f79ce
Àlex Serra <bounteous17@protonmail.com>
2024-08-28 23:07:08 +0200

    ci: better dotenv setup

--- test/cli.e2e-spec.js
+++ test/cli.e2e-spec.js
@@ -1,26 +1,29 @@
 const { v2: compose } = require('docker-compose');
-const { createClient } = require('redis');
+const { createClient: createRedisClient } = require('redis');
 
 describe('e2e', () => {
     it('should execute provided function', async () => {
         await compose.buildOne('warthog', { cwd: './example' });
         await compose.upMany(['warthog', 'redis'], { cwd: './example', log: true });
 
-        const client = createClient();
-        await client.connect();
-        await client.flushAll();
+        const clients = {
+            redis: createRedisClient()
+        };
 
-        const getAllKeys = async () => client.keys('*');
+        await clients.redis.connect();
+        await clients.redis.flushAll();
+
+        const getAllKeys = async () => clients.redis.keys('*');
 
         const initialKeys = await getAllKeys();
         expect(initialKeys).toHaveLength(0);
 
-        await compose.exec('warthog', 'pnpm exec warthog', { cwd: './example', log: true });
+        await compose.exec('warthog', 'pnpm start', { cwd: './example', log: true });
 
         const finalKeys = await getAllKeys();
         expect(finalKeys).toHaveLength(32);
 
-        await client.disconnect();
+        await clients.redis.disconnect();
         await compose.down({ cwd: './example' });
     });
 });
